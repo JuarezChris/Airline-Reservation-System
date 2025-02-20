01bf52135b5791d15298a07ca2179fb31efbb5e2
Christopher Juarez <juarez.christopher@yahoo.com>
2025-02-19 19:06:32 -0600

    adding frontend

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,16 +1,38 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useState, useEffect } from "react";
+import axios from "axios";
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [data, setData] = useState([]);
 
-  return (
-    <>
-      Helloo
-    </>
-  )
+    useEffect(() => {
+      axios.get("http://127.0.0.1:5000/data", {withCredentials:true, headers: {
+        "Content-Type": "application/json", // Ensure JSON is sent
+        "Accept": "application/json", // Expect JSON response
+      },})
+      .then(response => {
+          console.log("Received Data:", response.data);  // Debug here
+          if (typeof response.data === "string") {
+              setData(JSON.parse(response.data));  // Manually parse if needed
+          } else {
+              setData(response.data);
+          }
+      })
+      .catch(error => console.error("Error fetching data:", error));
+    }, []);
+
+    return (
+        <div>
+            <h1>CSV Data from Flask</h1>
+            <ul>
+                {data.map((item) => (
+                    <li key={item.id}>
+                        {item.name}
+                    </li>
+                ))}
+            </ul>
+        </div>
+    );
 }
 
-export default App
+export default App;
